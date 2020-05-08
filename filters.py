e61e0bdcc4078dc915c461f76ecce1441644e941
Ruan Montelo <ruanmontelo@gmail.com>
2020-05-08 19:39:33 -0300

    Fixing description and bug that was filtering every six days instead of five

--- filters.py
+++ filters.py
@@ -7,7 +7,7 @@ def filter_every_five_items(list_to_filter_from: list) -> list:
     counter = 5
     for item in list_to_filter_from:
         if counter == 5:
-            counter = 0
+            counter = 1
             filtered_list.append(item)
         else:
             counter = counter + 1
--- main.py
+++ main.py
@@ -16,6 +16,6 @@ with open('owid-covid-data.csv') as csv_file:
     list_of_cases = filter_every_five_items(list_of_cases)
 
 plt.plot(list_of_cases, list_of_dates, "#FF0000")
-plt.ylabel("Total de casos")
-plt.xlabel("Datas")
+plt.xlabel("Total de casos (sem dados, recuperados e mortos)")
+plt.ylabel("Datas")
 plt.show()
