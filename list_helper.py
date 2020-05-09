f96784e12f1a5783ecb2fe0fe71583f1d2b00f31
Ruan Montelo <ruanmontelo@gmail.com>
2020-05-09 13:09:33 -0300

    Fixing null checks and length check for lists in the ListHelper class

--- list_helper.py
+++ list_helper.py
@@ -2,18 +2,19 @@ class ListHelper:
 
     @staticmethod
     def format_lists_from_all_covid_cases_list(covid_cases_filtered_by_country: list, list_of_dates: list, list_of_cases: list):
-        if (len(covid_cases_filtered_by_country) > 0):
+        if (type(covid_cases_filtered_by_country) is not None and len(covid_cases_filtered_by_country) > 0):
             for row in covid_cases_filtered_by_country:
                 try:
                     list_of_dates.append(row[2][5:])
                     list_of_cases.append(row[3])
-                except:
+                except Exception as exception_message:
+                    print(f'Failed on converting the current row. Error: {exception_message}')
                     pass
 
     @staticmethod
     def filter_by_country(list_of_covid_cases_to_filter: list, country: str) -> list:
         filtered_list = []
-        if (len(list_of_covid_cases_to_filter) > 0):
+        if (list_of_covid_cases_to_filter is not None and len(list_of_covid_cases_to_filter) > 0):
             for row in list_of_covid_cases_to_filter:
                 if row[0] == country.upper():
                     filtered_list.append(row)
@@ -21,7 +22,7 @@ class ListHelper:
 
     @staticmethod
     def get_cases_by_interval_in_days(list_to_filter_from: list, interval_in_days: int) -> list:
-        if (len(list_to_filter_from)):
+        if (type(list_to_filter_from) is not None and len(list_to_filter_from) > 0):
             filtered_list = []
             interval_counter = interval_in_days
             reset_days_counting = 1
