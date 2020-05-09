26f5fc4629da975ccfaea75c0beabda1b2f0fc76
Ruan Montelo <ruanmontelo@gmail.com>
2020-05-09 01:38:15 -0300

    Initiating refactoring and separation of concerns for each variable/function with the objective of letting the user interact with the script and generating their own case visualization

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import csv
 import matplotlib.pyplot as plt
-import numpy as np
-from filters import filter_by_country, filter_every_five_items
+from filter_functions import filter_by_country, get_cases_by_number_of_days
+from covid_cases import get_all_covid_cases_from_csv
 
 list_of_dates = []
 list_of_cases = []
 
-with open('owid-covid-data.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, dialect='excel',  delimiter=',')
-    filtered_cases = filter(filter_by_country, csv_reader)
-    for row in filtered_cases:
-        list_of_dates.append(row[2])
+chosen_country_iso_code = input("Please write the iso_code for the country of preference (e.g: BRA, USA, ITA): ")
+covid_cases_filtered_by_country = filter_by_country(get_all_covid_cases_from_csv(), chosen_country_iso_code)
+for row in covid_cases_filtered_by_country:
+    try:
+        list_of_dates.append(row[2][5:])
         list_of_cases.append(row[3])
-    list_of_dates = filter_every_five_items(list_of_dates)
-    list_of_cases = filter_every_five_items(list_of_cases)
+    except Exception as exception_message:
+        print(f"Couldn't convert row because of the following error: {exception_message}")
 
-plt.plot(list_of_cases, list_of_dates, "#FF0000")
-plt.xlabel("Total de casos (sem dados, recuperados e mortos)")
-plt.ylabel("Datas")
+number_of_days_to_show_data = int(input("Please input the number of days to between cases (only numbers): "))
+list_of_dates = get_cases_by_number_of_days(list_of_dates, number_of_days_to_show_data)
+list_of_cases = get_cases_by_number_of_days(list_of_cases, number_of_days_to_show_data)
+
+plt.plot(list_of_dates, list_of_cases, "#FF0000")
+plt.ylabel("Total de casos confirmados (testes, recuperados, mortos)")
+plt.xlabel(f"Data (cada {number_of_days_to_show_data} dias)")
 plt.show()
