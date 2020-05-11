7de23eae3cae116a060136b361858f52effd88a4
Ruan Montelo <ruanmontelo@gmail.com>
2020-05-11 10:02:53 -0300

    Separating the function that managed both lists to build the graph into two functions and not declaring the lists until theyre used in the main.py file

--- list_manager.py
+++ list_manager.py
@@ -3,17 +3,6 @@ class ListManager:
     def __init__(self):
         pass
 
-    def format_lists_from_all_covid_cases_list(self, covid_cases_filtered_by_country: list, list_of_dates: list, list_of_cases: list, type_of_covid_data: int):
-        if (len(covid_cases_filtered_by_country) > 0):
-            for row in covid_cases_filtered_by_country:
-                try:
-                    # [5:] -> Substring to get only 'month-day' instead of the full date format
-                    list_of_dates.append(row[covid_csv_index_constants.LIST_DATE_INDEX][5:])
-                    list_of_cases.append(row[type_of_covid_data])
-                except Exception as exception_message:
-                    print(f'Failed on converting the current row. Error: {str(exception_message)}')
-                    pass
-
     def filter_by_country_iso_code(self, list_of_covid_cases_to_filter: list, country_iso_code: str) -> list:
         filtered_list = []
         if (len(list_of_covid_cases_to_filter) > 0):
@@ -21,12 +10,33 @@ class ListManager:
                 if row[0] == country_iso_code:
                     filtered_list.append(row)
             return filtered_list
-        else:
-            return []
+        return filtered_list
+
+    def get_type_of_data_list(self, covid_cases_filtered_by_country: list, type_of_covid_data: int):
+        list_of_cases = []
+        if (len(covid_cases_filtered_by_country) > 0):
+            for row in covid_cases_filtered_by_country:
+                try:
+                    list_of_cases.append(row[type_of_covid_data])
+                except Exception as exception_message:
+                    print(f'Failed on converting the current row. Error: {exception_message}')
+                    pass
+        return list_of_cases
+
+    def get_date_list(self, covid_cases_filtered_by_country: list):
+        list_of_dates = []
+        if (len(covid_cases_filtered_by_country) > 0):
+            for row in covid_cases_filtered_by_country:
+                try:
+                    # [5:] -> Substring to get only 'month-day' instead of the full date format
+                    list_of_dates.append(row[covid_csv_index_constants.LIST_DATE_INDEX][5:])
+                except Exception as exception_message:
+                    print(f'Failed on converting the current date row. Error: {exception_message}')
+        return list_of_dates
 
     def get_cases_by_interval_in_days(self, list_to_filter_from: list, interval_in_days: int) -> list:
+        filtered_list = []
         if (len(list_to_filter_from) > 0):
-            filtered_list = []
             interval_counter = interval_in_days
             reset_days_counting = 1
             for row in list_to_filter_from:
@@ -36,5 +46,4 @@ class ListManager:
                 else:
                     interval_counter = interval_counter + 1
             return filtered_list
-        else:
-            return []
+        return filtered_list
--- main.py
+++ main.py
@@ -7,16 +7,14 @@ listManager = ListManager()
 csvManager = CsvManager()
 uiManager = UiManager()
 
-list_of_dates = []
-list_of_cases = []
-
 inputManager.prompt_user_for_country_iso_code()
 inputManager.prompt_user_for_type_of_data()
 inputManager.prompt_user_for_interval_in_days()
 
 covid_cases_filtered_by_country = listManager.filter_by_country_iso_code(csvManager.ALL_COVID_CASES_IN_CSV, inputManager.chosen_country_iso_code)
 
-listManager.format_lists_from_all_covid_cases_list(covid_cases_filtered_by_country, list_of_dates, list_of_cases, inputManager.chosen_type_of_data)
+list_of_dates = listManager.get_date_list(covid_cases_filtered_by_country)
+list_of_cases = listManager.get_type_of_data_list(covid_cases_filtered_by_country, inputManager.chosen_type_of_data)
 
 list_of_dates = listManager.get_cases_by_interval_in_days(list_of_dates, inputManager.chosen_interval_in_days)
 list_of_cases = listManager.get_cases_by_interval_in_days(list_of_cases, inputManager.chosen_interval_in_days)
